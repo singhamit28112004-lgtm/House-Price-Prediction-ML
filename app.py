3fd3b209e2ef6f27d46d49e4ce36d5bbeaf26ae5
Amit Pratap Singh <singhamit28112004@gmail.com>
2025-12-28 15:20:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 st.image('https://cdn.dribbble.com/userupload/23217976/file/original-203940eb89dec42ee25f6af662af24dc.gif')
 df = pd.read.csv('house_data_csv')
-X = df.iloc[:,-3:]
+X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
 st.sidebar.title('Select House features')
@@ -19,5 +19,6 @@ for i in X:
  all_value.append(ans)
 
 st.write(all_value)
+
 
 
