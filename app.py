c8d58e97557bdc1100ae253d96bb795fc286d7e3
Amit Pratap Singh <singhamit28112004@gmail.com>
2025-12-28 16:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
-
-st.title('🏠House Price prediction using ML')
+st.title('🏠 House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
 
 df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-st.sidebar.title('Select House features')
+st.sidebar.title('🏠 Select House features')
 st.sidebar.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
 all_value = []
 for i in X:
@@ -28,14 +27,18 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
 
+@st.cache_data
+def model_run(X,y):
+  model = RandomForestRegressor()
+  model.fit(X,y)
+  return model
 
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+model = model_run(X,y)  
 house_price = model.predict(final_value)[0]
 
-with st.spinner('Predicting House Price'):
+with st.spinner('Predicting House price'):
   time.sleep(1)
-  msg = f'''House price is: $ {round(house_price*180500,2)}'''
+msg = f'''House price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 
-st.markdown('''**Design and Develop by: Amit Pratap Singh**''')
+st.markdown('''**Design and Developed by: Ankush verma**''')
